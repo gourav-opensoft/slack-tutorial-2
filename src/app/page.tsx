46cb74f0113ce5fa217a26ce7691ca9a9970c694
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-03-04 18:36:39 +0530

    feat: Implement workspace fetching, user authentication UI, and core Convex schema and API.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@
 
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useMemo } from "react";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+
 import { useEffect } from "react";
+
 import { useRouter } from "next/navigation";
 import { useConvexAuth } from "convex/react";
 
 export default function Home() {
+  const { data, isLoading: isLoadingWorkspaces } = useGetWorkspaces();
+
   const router = useRouter();
   const { isAuthenticated, isLoading } = useConvexAuth();
 
@@ -16,13 +22,25 @@ export default function Home() {
     }
   }, [isLoading, isAuthenticated, router]);
 
-  if (isLoading) {
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else {
+      console.log("open creation modal");
+    }
+  }, [workspaceId, isLoading]);
+
+  if (isLoading || isLoadingWorkspaces) {
     return <div>Loading...</div>;
   }
 
   return (
-      <div>
-        <UserButton />
-      </div>
+    <div>
+      <UserButton />
+    </div>
   );
-};
+}
