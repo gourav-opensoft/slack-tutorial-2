0d92294dd7bd743796fe4b41e6f0175c560f7a42
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-03-30 18:23:56 +0530

    feat: implement resizable workspace layout using react-resizable-panels

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,40 +1,60 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import { useCurrentUser } from "@/features/auth/api/use-current-user";
 
-import { Toolbar } from "./toolbar";
 import { Sidebar } from "./sidebar";
+import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-    const router = useRouter();
-    const { data: currentUser, isLoading } = useCurrentUser();
+  const router = useRouter();
+  const { data: currentUser, isLoading } = useCurrentUser();
 
-    useEffect(() => {
-        if (!isLoading && !currentUser) {
-            router.replace("/login-page");
-        }
-    }, [currentUser, isLoading, router]);
-
-    if (isLoading) {
-        return null; // Or a loader
+  useEffect(() => {
+    if (!isLoading && !currentUser) {
+      router.replace("/login-page");
     }
+  }, [currentUser, isLoading, router]);
 
-    return (
-        <div className="h-full">
-            <Toolbar />
-            <div className="flex h-[calc(100vh-40px)]">
-                <Sidebar />
-            {children}
-            </div>
-        </div>
-    );
+  if (isLoading) {
+    return null; // Or a loader
+  }
+
+  return (
+    <div className="h-full">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </div>
+  );
 };
 
 export default WorkspaceIdLayout;
