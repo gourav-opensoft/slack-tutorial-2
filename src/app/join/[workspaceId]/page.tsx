d737a39d60853d621d5852df88c9b42ec9375a57
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-04-18 18:46:09 +0530

    feat: add workspace join page with verification input and logo asset

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,20 +1,53 @@
 "use client";
 
+import { use } from "react";
+import Link from "next/link";
 import Image from "next/image";
+import VerificationInput from 'react-verification-input';
 
-const JoinPage = () => {
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+
+const JoinPage=() => {
+  const workspaceId = useWorkspaceId();
+
+  const {} = useGetWorkspace
+}
   return (
     <div className=" h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
       <Image src="/logo.svg" width={100} height={60} alt="Logo" />
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-          <h1>
+          <h1 className="text-2xl font-bold">
             Join workspace
           </h1>
-          <p>
-            enter the workspace code to join
+          <p className="text-md text-muted-foreground">
+            Enter the workspace code to join
           </p>
         </div>
+        <VerificationInput
+          length={6}
+          classNames={{
+            container: "flex gap-x-2",
+            character: "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black"
+          }}
+          autoFocus
+        />
+      </div>
+      <div className="flex gap-x-4">
+        <Button
+          size="lg"
+          variant="outline"
+          asChild
+        >
+          <Link href="/">
+            Back to home
+          </Link>
+        </Button>
       </div>
     </div>
   );
