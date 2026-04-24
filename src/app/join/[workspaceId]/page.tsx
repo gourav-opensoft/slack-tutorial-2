eb47db6aebafebc8e86eefab3d82e3488ddc5ab3
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-04-24 18:48:41 +0530

    feat: implement workspace join functionality with invite code verification

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,19 +1,54 @@
 "use client";
 
-import { use } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { toast } from 'sonner';
+import { useMemo, useEffect } from 'react';
+import { Loader } from "lucide-react";
+import { useRouter } from 'next/navigation';
 import VerificationInput from 'react-verification-input';
 
+import { useJoin } from '@/features/workspaces/api/use-join';
+import { useGetWorkspaceInfo } from '@/features/workspaces/api/use-get-workspace-info';
+
+import { cn } from '@/lib/utils';
 import { Button } from "@/components/ui/button";
-import { Loader } from "lucide-react";
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
-import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
 const JoinPage = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
   
-  const {}: useGetWorkspace
+  const { mutate, isPending } = useJoin();
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  const isMember = useMemo(() => data?.isMember, [data?.isMember]);
+
+  useEffect(() => {
+    if (isMember) {
+      router.push(`/workspace/${workspaceId}`);
+    }
+  }, [isMember, router, workspaceId]);
+
+  const handleComplete = (value: string) => {
+    mutate({ workspaceId, joinCode: value }, {
+      onSuccess: (id) => {
+        router.push(`/workspace/${id}`);
+        toast.success("Workspace joined");
+      },
+      onError: () => {
+        toast.error("Failed to join workspace")
+      }
+    })
+  };
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-10 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
 
   return (
 
@@ -22,16 +57,17 @@ const JoinPage = () => {
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
           <h1 className="text-2xl font-bold">
-            Join workspace
+            Join {data?.name}
           </h1>
           <p className="text-md text-muted-foreground">
             Enter the workspace code to join
           </p>
         </div>
         <VerificationInput
+          onChange={handleComplete}
           length={6}
           classNames={{
-            container: "flex gap-x-2",
+            container: cn("flex gap-x-2", isPending && "opacity-50 cursor-not-allowed"),
             character: "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
             characterInactive: "bg-muted",
             characterSelected: "bg-white text-black",
