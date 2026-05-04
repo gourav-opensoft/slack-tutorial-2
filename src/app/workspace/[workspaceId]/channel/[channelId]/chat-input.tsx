2387013e1aa5a31983b0272227970b6e7693985e
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-05-04 19:52:16 +0530

    feat: add Quill-based editor component and set up workspace chat structure

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,7 +1,24 @@
-export const ChatInput = () => {
+import Quill from "quill";
+import { useRef } from "react";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
+
+interface ChatInputProps {
+    placeholder?: string;
+};
+
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
+    const editorRef = useRef<Quill | null>(null);
+
     return (
         <div className="px-5 w-full">
-            <Editor />
+            <Editor
+               placeholder={placeholder}
+               onSubmit={() => {}}
+               disabled={false}
+               innerRef={editorRef}
+            />
         </div>
     );
-};
+};  
