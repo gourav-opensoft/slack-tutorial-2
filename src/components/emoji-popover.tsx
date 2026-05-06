77fbe0bc85df806623fe104691b676f1e4c20a82
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-05-06 18:28:57 +0530

    feat: implement rich text editor with image uploads and emoji picker support

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+
 import {
     Popover,
     PopoverContent,
@@ -18,12 +22,42 @@ interface EmojiPopoverProps {
 
 export const EmojiPopover = ({
     children,
-    hint,
+    hint = "Emoji",
     onEmojiSelect,
 }: EmojiPopoverProps) => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    const onSelect = (emoji: any) => {
+        onEmojiSelect?.(emoji);
+        setPopoverOpen(false);
+
+        setTimeout(() => {
+            setTooltipOpen(false);
+        }, 500);
+    };
+
     return (
-        <div>
-            {children}
-        </div>
+        <TooltipProvider>
+            <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+                <Tooltip
+                    open={tooltipOpen}
+                    onOpenChange={setTooltipOpen}
+                    delayDuration={50}
+                >
+                    <PopoverTrigger asChild>
+                        <TooltipTrigger asChild>
+                            {children}
+                        </TooltipTrigger>
+                    </PopoverTrigger>
+                    <TooltipContent className="bg-black text-white border border-white/5">
+                        <p className="font-medium text-xs">{hint}</p>
+                    </TooltipContent>
+                </Tooltip>
+                <PopoverContent className="p-0 w-full border-none shadow-none">
+                    <Picker data={data} onEmojiSelect={onSelect} />
+                </PopoverContent>
+            </Popover>
+        </TooltipProvider>
     );
 };
