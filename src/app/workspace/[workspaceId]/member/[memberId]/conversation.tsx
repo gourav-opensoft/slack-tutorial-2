dfcd3819c01dee6eb066601a00849ad842513f13
gaurav-opensoft <gouravlifedesign@zohomail.in>
2026-05-20 19:18:57 +0530

    feat: implement direct messaging system with member lookups and conversation retrieval logic

---
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -0,0 +1,11 @@
+import { Id } from "../../../../../../convex/_generated/dataModel";
+
+interface ConversationProps {
+    id: Id<"conversations">;
+}
+
+export const Conversation = ({ id }: ConversationProps) => {
+    return (
+        <div></div>
+    );
+};
